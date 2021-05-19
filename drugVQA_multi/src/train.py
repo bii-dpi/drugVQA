333fe7c6b6447353cdbef95f6fdea1f5665ac32c
bii-dpi <dummy@dummy.com>
2021-05-20 03:52:44 +0800

    Removed old drugVQA reps

--- drugVQA_multi/src/train.py
+++
@@ -1,8 +0,0 @@
-import torch
-from sklearn import metrics
-import warnings
-warnings.filterwarnings("ignore")
-from trainAndTest import *
-
-if __name__ == "__main__":
-    mp.spawn(train, nprocs=args.gpus, args=(trainArgs,))
