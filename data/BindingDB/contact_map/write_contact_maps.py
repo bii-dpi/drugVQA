78736a319370f19718e13e7a4974a5051c7ff5f3
bii dpi <76100319+bii-dpi@users.noreply.github.com>
2021-06-20 13:24:30 +0800

    ..

--- data/BindingDB/contact_map/write_contact_maps.py
+++ data/BindingDB/contact_map/write_contact_maps.py
@@ -1,37 +1,72 @@
 import numpy as np
 import pandas as pd
 
-from urllib.request import urlopen
-from progressbar import progressbar
 from Bio.PDB import *
+from progressbar import progressbar
 
 
-parser = PDBParser(PERMISSIVE=1)
+p = PDBParser(PERMISSIVE=1)
 
-def get_pdb_file(sequence):
-    pdb_id = sequence_mapping[sequence].split(",")[0]
+
+def get_pdb_id(sequence):
+    return sequence_mapping[sequence].split(",")[0]
+
+
+def get_pdb_file(pdb_id):
     try:
-        return parser.get_structure(pdb_id,
-                                    f"pdb_files/pdb{pdb_id.lower()}.ent")
+        return p.get_structure(pdb_id,
+                               f"pdb_files/pdb{pdb_id.lower()}.ent")
     except:
         raise Exception(f"{pdb_id} has no file.")
 
 
-def get_coord_list(residue):
-    pass
+def get_coord_list(structure):
+    coord_list = []
+
+    for model in structure.get_models():
+        for chain in model:
+            for residue in chain:
+                if residue.get_full_id()[-1][0] != " ":
+                    continue
+                for atom in residue:
+                    if atom.get_name().startswith("CA"):
+                        coord_list.append(atom.get_coord())
+                        break
+        break
+
+    return np.array(coord_list)
+
+
+def get_distance(coords_1, coords_2):
+    return 1 / (1 + (np.linalg.norm(coords_1 - coords_2) / 3.8))
 
 
 def get_distance_matrix(coord_list):
-    pass
+    dist_matrix = np.zeroes((len(coord_list), len(coord_list))
+    for i in range(len(coord_list)):
+        for j in range(len(coord_list)):
+            dist_matrix[i, j] = get_distance(coord_list[i],
+                                             coord_list[j])
+    return dist_matrix
+
+
+def get_matrix_string(pdb_id, sequence, dist_matrix):
+    text = f"{pdb_id}\n{sequence}"
+    for i in range(dist_matrix.shape[0]):
+        text += "\n"
+        for j in range(dist_matrix.shape[0]):
+            text += f"{dist_matrix[i, j]} "
+    return text
 
 
 def save_contact_map(sequence):
-    pdb_file  = get_pdb_file(sequence)
-    """
-    coord_list = get_coord_list(pdb_file)
-    return get_distance_matrix(coord_list)
-    """
-
+    pdb_id = get_pdb_id(sequence)
+    structure = get_pdb_file(pdb_id)
+    coord_list = get_coord_list(structure)
+    dist_matrix = get_distance_matrix(coord_list)
+    text = get_matrix_string(pdb_id, sequence, dist_matrix)
+    with open(pdb_id, "w") as f:
+        f.write(text)
 
 # Get BindingDB sequences and PDB IDs.
 bindingdb_sequences = pd.read_pickle("../data_pre/mapped_bindingdb_sequences.pkl")
@@ -39,6 +74,5 @@ bindingdb_sequences = [element.split("\n")[2] for element in bindingdb_sequences
 
 sequence_mapping = pd.read_pickle("sequence_to_id_map.pkl")
 
-
-for sequence in progressbar(bindingdb_sequences[:1]):
+for sequence in progressbar(bindingdb_sequences):
     save_contact_map(sequence)
