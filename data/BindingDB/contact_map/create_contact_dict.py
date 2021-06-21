16810f0b4d319b71bd4796313f424ae59b17df3f
bii dpi <76100319+bii-dpi@users.noreply.github.com>
2021-06-21 14:24:43 +0800

    Revised dict

--- data/BindingDB/contact_map/create_contact_dict.py
+++ data/BindingDB/contact_map/create_contact_dict.py
@@ -1,6 +1,23 @@
 import os
 import pandas as pd
 
+
+def get_seq(fname):
+    with open(fname, "r") as f:
+        return f.read().split("\n")[1]
+
+
+cm_files = [fname for fname in os.listdir()
+            if fname.endswith("_cm")]
+
+text = ""
+for fname in cm_files:
+    text += f"{get_seq(fname)}:{fname}\n"
+
+with open("BindingDB-contactDict", "w") as f:
+    f.write(text)
+
+"""
 contact_dict = pd.read_pickle("sequence_to_id_map.pkl")
 contact_dict = {sequence: ids.split(",")[0] for
                 sequence, ids in contact_dict.items()}
@@ -12,4 +29,4 @@ for sequence in contact_dict.keys():
 
 with open("BindingDB-contactDict", "w") as f:
     f.write(text)
-
+"""
