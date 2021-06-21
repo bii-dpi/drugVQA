121ee45817176553caf9fd26dbfd8151433f8e01
bii dpi <76100319+bii-dpi@users.noreply.github.com>
2021-06-21 11:48:01 +0800

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -146,8 +146,7 @@ def getSeqContactDict(contactPath, contactDictPath):# make a seq-contactMap dict
         _, contactMapName = data.strip().split(':')
         seq, contactMap = getProtein(contactPath, contactMapName)
         contactmap_np = [list(map(float, x.strip(' ').split(' '))) for x in contactMap]
-        print(contactmap_np)
-        feature2D = np.expand_dims(contactmap_np, axis=0)
+        feature2D = np.array(np.expand_dims(contactmap_np, axis=0), dtype=float)
         try:
             feature2D = torch.FloatTensor(feature2D)
         except:
