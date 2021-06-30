5d8f80c5467bc0001f0f191d609e5aeab160122d
bii dpi <76100319+bii-dpi@users.noreply.github.com>
2021-06-30 14:25:46 +0800

    ..

--- src/deploy_bindingdb_test.py
+++ src/deploy_bindingdb_test.py
@@ -1,8 +1,8 @@
 import os
 
-for seed_index in range(0, 3):
-	for fold_num in range(1, 4):
-		print(seed_index, fold_num)
-		# RV_SEED_INDEX, SEED_INDEX, FOLD_TYPE, FOLD_NUM, CUDA_NUM
-		os.system(f"python test_bindingdb_single.py 0 {seed_index} rv {fold_num} 0")
+seed_index = 0
+for fold_num in range(1, 4):
+    print(seed_index, fold_num)
+    # RV_SEED_INDEX, SEED_INDEX, FOLD_TYPE, FOLD_NUM, CUDA_NUM
+    os.system(f"python test_bindingdb_single.py 0 {seed_index} rv {fold_num} {seed_index}")
 
