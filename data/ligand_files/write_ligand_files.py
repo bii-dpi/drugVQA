13c3f30f7074aba3fe3c61e060d99179cab3bad7
bii-dpi <>
2021-07-05 10:47:28 +0800

    ..

--- data/ligand_files/write_ligand_files.py
+++ data/ligand_files/write_ligand_files.py
@@ -4,7 +4,9 @@ import pickle
 import numpy as np
 
 from rdkit import Chem
+from rdkit.Chem import AllChem
 from progressbar import progressbar
+from concurrent.futures import ProcessPoolExecutor
 
 
 def get_smiles_strings(path):
@@ -13,10 +15,24 @@ def get_smiles_strings(path):
     return np.unique([example.split()[0] for example in examples])
 
 
-def write_sdf_file(smiles_string, i):
+def write_mol_file(smiles_string, i):
     mol = Chem.MolFromSmiles(smiles_string)
+
+    mol_with_H = Chem.AddHs(mol)
+
+    AllChem.EmbedMolecule(mol_with_H)
+    AllChem.MMFFOptimizeMolecule(mol_with_H)
+
+    embedded_mol = Chem.RemoveHs(mol_with_H)
+    Chem.MolToMolFile(embedded_mol, f"{i}.mol")
+
+    """
+    with open(f"{i}.mol", "w") as f:
+        f.write(mol)
+        f.write(Chem.MoltoMolBlock(mol))
     with Chem.rdmolfiles.SDWriter(f"{i}.mol") as w:
         w.write(Chem.MoltoMolBlock(mol))
+    """
 
 
 if "smiles_dict.pkl" in os.listdir():
@@ -42,16 +58,16 @@ if smiles_dict.keys():
 
 
 for smiles_string in progressbar(strings_to_do):
-    write_sdf_file(smiles_string, i)
+    write_mol_file(smiles_string, i)
     smiles_dict[i] = smiles_string
     i += 1
 
 with open("smiles_dict.pkl", "wb") as f:
     pickle.dump(smiles_dict, f)
 
-"""
-    #Writes the MolBlock to a .mol file
-    with open(f"../mols/{name}.mol", "w") as newfile:
-        newfile.write(imported)
-"""
+
+
+if __name__ == "__main__":
+    with ProcessPoolExecutor() as executor:
+        results = executor.map(write_mol_file, strings_to_do)
 
