ea575cefc5812d09e2297093f5acf52fb3df171d
bii-dpi <>
2021-07-15 11:06:24 +0800

    Did minor cleaning of DUD-E contact maps, only use contact dict for seq-to-id:

--- data/DUDE/contact_map/utils_download_pdb_files.py
+++ data/DUDE/contact_map/utils_download_pdb_files.py
@@ -5,10 +5,8 @@ from Bio.PDB import *
 
 pdbl = PDBList()
 
-pdb_ids = [fname.split("_")[1][:-1] for fname in os.listdir()
-           if fname.endswith("_full")]
-
-print(pdb_ids)
+pdb_ids = [fname.split("_")[0] for fname in os.listdir()
+           if fname.endswith("_cm")]
 
 pdbl.download_pdb_files(pdb_ids, file_format="pdb", pdir="pdb_files")
 
