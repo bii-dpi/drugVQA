a6baa3ce959605581d1768eb5e3a0ed95391de03
bii-dpi <>
2021-07-17 12:48:51 +0800

    Did some more whittling

--- data/BindingDB/contact_map/00_download_pdb_files.py
+++ data/BindingDB/contact_map/00_download_pdb_files.py
@@ -7,12 +7,27 @@ from Bio.PDB import *
 from pickle import dump
 
 
-def get_shortest(sequence_list):
-    return sequence_list[np.argmin([len(sequence) for sequence in sequence_list])]
+def get_num_examples(sequence):
+    return len([other_sequence
+                for other_sequence in raw_sequences
+                if other_sequence == sequence])
+
+
+def get_sequence(sequence_list):
+    sequence_num_examples = [get_num_examples(sequence) for sequence in sequence_list]
+    if max(sequence_num_examples) < 40:
+        return None
+    return sequence_list[np.argmax(sequence_num_examples)]
 
 
 # Read in sequences and PDB IDs.
 bindingdb_examples = pd.read_csv("bindingdb_examples_raw.tsv", sep = "\t")
+
+## Read in all sequences without dropping duplicates.
+raw_sequences = [sequence.upper().strip().strip("\n").strip("\t")
+                 for sequence in bindingdb_examples.target_sequence.dropna()]
+
+## Create the sequence-PDB ID mapping.
 bindingdb_examples = \
     bindingdb_examples[bindingdb_examples.columns
                         [bindingdb_examples.columns.isin(["target_sequence",
@@ -26,16 +41,20 @@ raw_bindingdb_sequences = [sequence.upper().strip().strip("\n").strip("\t")
 sequence_to_id_map = dict(zip(raw_bindingdb_sequences,
                               [line.split(",")[0]
                                for line in bindingdb_examples.target_pdb_id.tolist()]))
+sequence_to_id_map = {sequence: pdb_id for sequence, pdb_id
+                      in sequence_to_id_map.items()
+                      if len(sequence) != 7096}
 
 selected_sequences = {pdb_id: [] for pdb_id in sequence_to_id_map.values()}
 for sequence in sequence_to_id_map.keys():
     selected_sequences[sequence_to_id_map[sequence]].append(sequence)
 
 for pdb_id in selected_sequences.keys():
-    selected_sequences[pdb_id] = get_shortest(selected_sequences[pdb_id])
+    selected_sequences[pdb_id] = get_sequence(selected_sequences[pdb_id])
 
 sequence_to_id_map = {sequence: pdb_id
-                      for pdb_id, sequence in selected_sequences.items()}
+                      for pdb_id, sequence in selected_sequences.items()
+                      if sequence is not None}
 
 
 # Download PDBs.
@@ -54,4 +73,6 @@ bindingdb_examples = bindingdb_examples[
                         target_sequence.isin(sequence_to_id_map.keys())
                      ]
 bindingdb_examples.to_csv("../data_pre/bindingdb_examples.csv")
+print(len(sequence_to_id_map))
+print(len(bindingdb_examples))
 
