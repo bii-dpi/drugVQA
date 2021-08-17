02f64868e5e1cd97e018e9e9554387b4a8d816a5
bii-dpi <>
2021-08-18 06:18:36 +0800

    ..

--- data/Shallow/03_fit_model.py
+++ data/Shallow/03_fit_model.py
@@ -1,31 +1,53 @@
+import numpy as np
 import pandas as pd
 import autogluon.core as ag
 from autogluon.tabular import TabularPredictor, TabularDataset
 from autogluon.core.metrics import make_scorer
 from sklearn.metrics import average_precision_score
+from progressbar import progressbar
+from concurrent.futures import ProcessPoolExecutor
 
 
-scorer = make_scorer("AUPR", average_precision_score, optimum=1, needs_proba=True)
+#scorer = make_scorer("AUPR", average_precision_score, optimum=1, needs_proba=True)
 
-def train(direction, resample_ratio):
-    print(f"Training in direction {direction} with ratio {resample_ratio}.")
-
-    print("Loading data...")
-    train_data = pd.read_csv(f"{direction}_{resample_ratio}_training_examples.csv")
+def get_data(direction, resample_ratio):
+    return pd.read_csv(f"{direction}_{resample_ratio}_training_examples.csv")
 
 
-    print("Fitting model...")
+def train(quartet):
+    direction, resample_ratio, random_seed, data = quartet
+
     predictor = TabularPredictor(problem_type="binary", label="Y",
-                                 eval_metric=scorer,
-                                 path=f"{direction}_{resample_ratio}")
-    predictor.fit(train_data=train_data,
-                  time_limit=11*60*60,
-                  presets="good_quality_faster_inference_only_refit",
-                  excluded_model_types=["NN", "NeuralNetFastAI",
-                                        "NNFastAiTabularModel",
-                                        "TabularNeuralNetModel"],
-                  verbosity=2)
+                                 eval_metric="f1",#scorer,
+                                 path=f"models/{direction}_{resample_ratio}_{random_seed}")
+    predictor.fit(train_data=data,
+                  #time_limit=2.5*60*60,
+                  presets="medium_quality_faster_train", #"good_quality_faster_inference_only_refit", #"medium_quality_faster_train"
+                  hyperparameters={
+                                     'KNN': {},
+                                     'GBM': {"seed": random_seed},
+                                     'RF': {"random_state": random_seed},
+                                     'CAT': {"random_seed": random_seed},
+                                     'XT': {"random_state": random_seed},
+                                     'XGB': {"random_state": random_seed},
+                                     'LR': {"random_state": random_seed}},
+                  verbosity=0)
 
-train("btd", 1)
-#train("dtb", 2)
 
+np.random.seed(12345)
+random_seeds = np.random.randint(0, 999999999, 10)
+
+print("Loading arguments...")
+quartets = []
+for direction in ["btde"]:# "btd", "dtb", "detb", "btde"]:
+    data = get_data(direction, 1)
+    for random_seed in random_seeds:
+        quartets.append([direction, 1, random_seed, data])
+
+print("Fitting model...")
+with ProcessPoolExecutor() as executor:
+    executor.map(train, quartets)
+"""
+for quartet in progressbar(quartets):
+    train(quartet)
+"""
--- data/Shallow/04_test.py
+++ data/Shallow/04_test.py
@@ -2,30 +2,45 @@ import numpy as np
 import autogluon.core as ag
 
 from sklearn import metrics
+from progressbar import progressbar
+from concurrent.futures import ProcessPoolExecutor
 from autogluon.tabular import TabularPredictor, TabularDataset
 
 
-SAMPLE_SIZE = 40000
+SAMPLE_SIZE = 50000
 
 
-def print_performance(direction, resampling_ratio):
+def get_data(direction, resampling_ratio):
     print(f"Testing in direction {direction} with resampling ratio {resampling_ratio}.")
     print("Loading data...")
-    data = TabularDataset(f"{direction}_{resampling_ratio}_testing_examples.csv")[:SAMPLE_SIZE]
-
-    print("Testing model...")
-    predictor = TabularPredictor.load(f"{direction}_{resampling_ratio}")
-    predictions = predictor.predict_proba(data)[1].tolist()
-
-    recall = metrics.recall_score(data.Y, np.round(predictions))
-    precision = metrics.precision_score(data.Y, np.round(predictions))
-    AUPR = metrics.average_precision_score(data.Y, predictions)
-
-    print(f"Precision: {precision}")
-    print(f"Recall: {recall}")
-    print(f"AUPR: {AUPR}")
+    return TabularDataset(f"{direction}_{resampling_ratio}_testing_examples.csv")[:SAMPLE_SIZE]
 
 
-print_performance("btd", 1)
-print_performance("dtb", 2)
+def save_performance(quartet):
+    direction, resampling_ratio, random_seed, data = quartet
+
+    predictor = TabularPredictor.load(f"models/{direction}_{resampling_ratio}_{random_seed}")
+
+    for model in predictor.get_model_names():
+        predictions = predictor.predict_proba(data, model)[1].tolist()
+
+        recall = metrics.recall_score(data.Y, np.round(predictions))
+        precision = metrics.precision_score(data.Y, np.round(predictions))
+        AUPR = metrics.average_precision_score(data.Y, predictions)
+
+        np.save(f"results/{direction}_{resampling_ratio}_{random_seed}_{model}.npy",
+                [recall, precision, AUPR])
+
+
+np.random.seed(12345)
+
+random_seeds = np.random.randint(0, 999999999, 10)
+
+for direction in progressbar(["btd", "dtb", "detb", "btde"]):
+    data = get_data(direction, 1)
+    quartets = []
+    for random_seed in random_seeds:
+        quartets.append([direction, 1, random_seed, data])
+    with ProcessPoolExecutor() as executor:
+        executor.map(save_performance, quartets)
 
